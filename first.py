3887216164a0dcbeb95d8cdb308612470c330b42
Sourabh277 <sorabh.talwar277@gmail.com>
2022-10-04 20:21:27 +0530

    frst code add

---
+++ first.py
@@ -0,0 +1,61 @@
+#Print Hello World basic
+#print ("Welcome to learn Python")
+# =================================================================
+
+#Print the full name and details
+#a = input ("Enter your Fisrt Name! ")
+#b = input ("Enter your Last Name! ")
+#c = input ("Enter your DOB in DD-MM-YYYY ")
+
+#print (a  +  b)
+#print ("Your DOB is " + (c) + ', Thanks to share details with us !! ')
+# ==================================================================
+
+# Number addtion after taking inputs from user!!!
+# a = input ("Enter the first number :")
+# b = input ("Enter the second number :")
+
+# a =int(a)
+# b =int(b)
+
+# print ('Addition of a & b is : ', a+b )
+# ===================================================================
+
+# Number substraction after taking inputs from user!!!
+#a = input ("Enter the first number :")
+#b = input ("Enter the second number :")
+
+#a =int(a)
+#b =int(b)
+
+#print ('Subtraction of a & b is : ', a-b )
+#====================================================================
+
+#a = input ("Enter the first number :")
+#b = input ("Enter the second number :")
+
+#a =int(a)
+#b =int(b)
+
+#print ('Multiplication of a & b is : ', a*b )
+#====================================================================
+
+#a = input ("Enter the Devidend number :")
+#b = input ("Enter the Devisor number :")
+
+#a =int(a)
+#b =int(b)
+
+#print ('Division of a & b is : ', a/b )
+#====================================================================
+
+#Remainder When b devided by a
+
+#a = input ("Enter the Devidend number :")
+#b = input ("Enter the Devisor number :")
+
+#a =int(a)
+#b =int(b)
+
+#print ('Remainder of a & b is : ', a%b )
+#=====================================================================
